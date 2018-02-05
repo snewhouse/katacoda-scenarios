8ea07e7bb9bc59cc8020f851c0e6c2c1aa7941b5
snewhouse <stephen.j.newhouse@gmail.com>
2018-02-05 16:34:16 +0000

    WIP

--- basic_linux_7BBG2014/courseBase.sh
+++ basic_linux_7BBG2014/courseBase.sh
@@ -1,5 +1,1 @@
-mkdir /home/ngs_user
-export HOME="/home/ngs_user"
-cd /home/ngs_user
-mkdir -p {docs, scripts, data, output, scratch}
-sudo apt-get install tree htop
+git clone https://github.com/snewhouse/katacoda-dummy-dir.git
