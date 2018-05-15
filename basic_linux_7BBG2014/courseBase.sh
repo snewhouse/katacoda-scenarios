c81a23d3b9a7b8f7d0e403f6583c11372b27d5cb
snewhouse <stephen.j.newhouse@gmail.com>
2018-05-15 09:24:27 +0100

    WIP

--- basic_linux_7BBG2014/courseBase.sh
+++ basic_linux_7BBG2014/courseBase.sh
@@ -1,4 +1,5 @@
 ssh root@host01 'sudo useradd -ms /bin/bash learner -p learner'
 ssh root@host01 'mkdir -p /home/learner/{a_directory,another_directory}'
 ssh root@host01 'sudo chown -R learner:learner /home/learner'
+sudo su - learner
 cd /home/learner
