500ca546f5cb6595993b1486aa1cd9063c8a89dd
snewhouse <stephen.j.newhouse@gmail.com>
2018-06-04 15:42:13 +0100

    WIP

--- basic_linux_7BBG2014/courseBase.sh
+++ basic_linux_7BBG2014/courseBase.sh
@@ -1,4 +1,6 @@
 ssh root@host01 'sudo useradd -ms /bin/bash learner'
 ssh root@host01 'mkdir -p /home/learner/{a_directory,another_directory}'
 ssh root@host01 'sudo chown -R learner:learner /home/learner'
+ssh root@host01 'sudo touch /home/learner/script.sh'
+ssh root@host01 'sudo chown -R learner:learner /home/learner/script.sh'
 ssh root@host01 'sudo apt-get install -y less htop'
