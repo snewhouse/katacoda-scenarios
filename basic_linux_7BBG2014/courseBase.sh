295947b71216201e7d51a0fbce28ed81bd290fe3
snewhouse <stephen.j.newhouse@gmail.com>
2018-06-04 14:40:49 +0100

    WIP

--- basic_linux_7BBG2014/courseBase.sh
+++ basic_linux_7BBG2014/courseBase.sh
@@ -1,3 +1,4 @@
 ssh root@host01 'sudo useradd -ms /bin/bash learner'
 ssh root@host01 'mkdir -p /home/learner/{a_directory,another_directory}'
 ssh root@host01 'sudo chown -R learner:learner /home/learner'
+ssh root@host01 'sudo apt-get install -y less htop'
