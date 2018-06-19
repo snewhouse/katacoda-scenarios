8a6c612048d1343582d09223e5ce1267df40a13b
snewhouse <stephen.j.newhouse@gmail.com>
2018-06-19 13:37:00 +0100

    WIP

--- basic_linux_7BBG2014/code.sh
+++ basic_linux_7BBG2014/code.sh
@@ -1,4 +1,3 @@
-#!/usr/bin/env bash
 sudo -i -u learner
-cd /home/learnerß
+cd /home/learner
 clear
--- basic_linux_7BBG2014/courseBase.sh
+++ basic_linux_7BBG2014/courseBase.sh
@@ -1,4 +1,4 @@
-ssh root@host01 'sudo useradd -p $(openssl passwd -1 learner) -mds /bin/bash learner'
+ssh root@host01 'sudo useradd -D -p $(openssl passwd -1 learner) -m -d -s /bin/bash learner'
 ssh root@host01 'mkdir -p /home/learner/{a_directory,another_directory}'
 #ssh root@host01 'sudo cp /home/packer/.bashrc /home/learner/'
 #ssh root@host01 'sudo cp /home/packer/.profile /home/learner/'
