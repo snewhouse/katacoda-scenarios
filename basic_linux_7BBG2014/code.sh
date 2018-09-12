9415c98b2929964114ad4aca45ab97ce3956500e
snewhouse <stephen.j.newhouse@gmail.com>
2018-09-12 15:11:52 +0100

    WIP

--- basic_linux_7BBG2014/code.sh
+++ basic_linux_7BBG2014/code.sh
@@ -1,4 +1,4 @@
-sudo userdel -r learner
+
 sudo useradd -p $(openssl passwd -1 learner) -s /bin/bash -G root -m -U learner
 sudo -i -u learner
 mkdir -p /home/learner/{a_directory,another_directory}
